fb09b3e3480c2e868bf1050b422f5a9359a30316
Erick-Rick <henrique.eryck@gmail.com>
2022-03-21 21:04:31 -0300

    Inicio do Projeto, configuração da NavBar

---
+++ ControleEstoque.Web/Controllers/OperacaoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class OperacaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult EntradaEstoque()
+        {
+            return View();
+        }
+        public IActionResult SaidaEstoque()
+        {
+            return View();
+        }
+        public IActionResult LancamentoPerdaProduto()
+        {
+            return View();
+        }
+        public IActionResult Inventario()
+        {
+            return View();
+        }
+    }
+}
